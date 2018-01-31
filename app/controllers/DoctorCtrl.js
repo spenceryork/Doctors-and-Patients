351e2dc190c3fc0df58c60a1f84293e5f376702f
Spencer York <spencerwyork@gmail.com>
2018-01-31 15:48:34 -0600

    doctors appear correctly on main page

--- app/controllers/DoctorCtrl.js
+++ app/controllers/DoctorCtrl.js
@@ -5,7 +5,14 @@ angular.module("doctorsAndPatients").controller("DoctorCtrl", function(FbFactory
     FbFactory.getDoctors()
     .then( (doctors) => {
         console.log("FB Doctor data", doctors);
-        $scope.doctors = "";
+        let doctorArr = [];
+        for (let i = 0; i < doctors.length; i++) {
+            let doctorName = Object.values(doctors[i]);
+            // doctorArr.push(doctors[i].first_name);
+            console.log("Doctor Name", doctorName);
+            doctorArr.push(doctorName[0]);
+        }
+        $scope.doctors = doctorArr;
     });
 
 });
